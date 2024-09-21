e91f0a92dad7a988a4927dd8cc87e9eb69881f36
Florian <flka00003@uni-saarland.de>
2024-09-22 01:12:34 +0200

    change main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from train_algorithms.ppo_trainer import PPO
-# import gymnasium as gym
+from environments.pygame_tetris import play_pygame
+
 import wandb
 import torch
 from config import Config
@@ -7,13 +8,29 @@ from config import Config
 
 device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
 if __name__ == '__main__':
-    wandb.login()
-    # env = gym.make("ALE/Tetris-v5")
-    config = Config(episodes_per_batch=128, updates_per_iteration=3, num_mini_batch_updates=32, num_sub_mini_batches=8, overall_timesteps=100000, lr=0.01)
-    ppo = PPO(device, config, experiment=1)
-    ppo.train(100000)
-
-    ppo.close()
 
 
+    try:
+        choice = input("Enter 'train' to train the AI or 'play' to watch the AI play: ")
+        if choice.lower() == 'train':
+            # train_ai(continue_training=True)
+            wandb.login()
+            config = Config(episodes_per_batch=128, updates_per_iteration=3, num_mini_batch_updates=32, num_sub_mini_batches=8, overall_timesteps=100000, lr=0.01)
+            ppo = PPO(device, config, experiment=1)
+            ppo.train(100000)
+
+            ppo.close()
+
+        elif choice.lower() == 'play':
+            player = input("Enter the filename {model.pth} to load the model from. Press ENTER to (empty string) to play as Human.")
+            play_pygame(player)
+        else:
+            print("Invalid choice. Please enter 'train' or 'play'.")
+    except EOFError:
+        print("No input received. Defaulting to training mode.")
+        # train_ai(continue_training=True)
+    except KeyboardInterrupt:
+        print("\nProgram interrupted by user. Exiting.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
     # env.close()
