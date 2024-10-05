d71936363097612e3b8da9d7d01d01047680e3d3
Sude Altinsoy <altinsoy156@gmail.com>
2024-10-05 16:57:31 +0200

    merged grid and tetronimo size

--- models/tetris_discrete_model.py
+++ models/tetris_discrete_model.py
@@ -3,9 +3,11 @@ import torch.nn as nn
 
 # AI implementation
 class TetrisAI(nn.Module):
-    def __init__(self, grid_size: tuple[int, int], tetronimo_size: int, action_space: int):
+    #def __init__(self, grid_size: tuple[int, int], tetronimo_size: int, action_space: int):
+    def __init__(self, input_size, action_space: int):
         super(TetrisAI, self).__init__()
-        input_size = grid_size[0] * grid_size[1] + tetronimo_size  # Deprecated: +8 for x, y, shape dimensions, bumpiness, holes, height, clearable_lines
+        #input_size = grid_size[0] * grid_size[1] + tetronimo_size  # Deprecated: +8 for x, y, shape dimensions, bumpiness, holes, height, clearable_lines
+
         # Now the input size is just the grid and the new_tile information.
         self.fc1 = nn.Linear(input_size, 128)
         self.fc2 = nn.Linear(128, 64)
