d4273b4993224dc739ab1f0ede979c7fe4675d68
Florian <flka00003@uni-saarland.de>
2024-11-05 16:36:10 +0100

    config descriptions

--- config.py
+++ config.py
@@ -11,6 +11,36 @@ class Config():
                 line_density_reward = 0.8,
                 height_place_reward = 0.1, height_penalty = 0.2,
                 bumpiness_penalty = 0.5, hole_penalty = 2, game_over_penalty = 500, info = "", predict_placement=False, load_model = -1):
+        """
+        Configuration class for setting hyperparameters for the training process.
+
+        Parameters:
+            episodes_per_batch (int): Number of episodes to run in parallel during training.
+            max_timesteps_per_episode (int): Maximum number of timesteps for each episode.
+            updates_per_iteration (int): Number of updates to perform per training iteration.
+            num_mini_batch_updates (int): Number of mini-batches to split the training data into for updates.
+            num_sub_mini_batches (int): Number of sub-batches for each mini-batch to control memory usage.
+            overall_timesteps (int): Total number of timesteps for training.
+            gamma (float): Discount factor for future rewards.
+            epsilon (float): Clipping parameter for the PPO objective.
+            lam (float): Lambda parameter for Generalized Advantage Estimation (GAE).
+            lr (float): Learning rate for the optimizer.
+            anneal_factor (float): Factor for learning rate annealing.
+            min_lr (float): Minimum learning rate to prevent it from going too low.
+            entropy_coef (float): Coefficient for the entropy bonus in the loss function.
+            vf_weight (float): Weight for the value function loss in the overall loss.
+            step_reward (float): Reward for each step taken in the environment.
+            line_clear_reward (float): Reward for clearing a line in the game.
+            line_density_reward (float): Reward based on the density of lines.
+            height_place_reward (float): Reward for placing pieces at certain heights.
+            height_penalty (float): Penalty for the height of the stack of pieces.
+            bumpiness_penalty (float): Penalty for the bumpiness of the stack.
+            hole_penalty (float): Penalty for creating holes in the stack.
+            game_over_penalty (float): Penalty for the game ending.
+            info (str): Additional information or notes.
+            predict_placement (bool): If True, the model predicts placements; otherwise, it predicts button actions.
+            load_model (int): Experiment number to load a pre-trained model from. Files must reside in `exports/` and have the form `ppo_conv_model_exp_{load_model}.pth`.
+        """
         # learning params
         self.episodes_per_batch = episodes_per_batch
         self.max_timesteps_per_episode = max_timesteps_per_episode
